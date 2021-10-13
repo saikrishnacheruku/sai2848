ebe94377a92063bbf92f59457940bd097471f5cd
saikrishna <saikrishnacheruku4884@gmail.com>
2021-10-13 15:31:36 +0530

    modules

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-const ul = document.querySelector('ul')
-const but = document.querySelector('button')
-const getText = document.querySelector('input')
-const parent = document.querySelector('div.grid')
+// Node.js program to demonstrate the
+// fs.writeFileSync() method
 
-const getData = async (item) => {
-    console.log(item)
-    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-    console.log(url)
-    const res = await fetch(url)
-    const data = await res.json()
-    const recipies = data.hits
-    recipies.forEach(item => {
-        const { recipe } = item
-        const { label, image, source, calories } = recipe
-        const template = ` 
-                        <img class="card__image" src="${image}" />
-                        <div class="card__data">
-                            <div class="card__info">
-                                <h2>${label}</h2>
-                                <p>From ${source}</p>
-                            </div>
-                            <h3 class="card__price"></h3>
-                            <button class="card__add">+</button>
-                        </div>
-                    `
-        const newCard = document.createElement('article')
-        newCard.setAttribute('class', 'card')
-        newCard.setAttribute('style', 'margin-bottom:20px')
-        newCard.innerHTML = template
-        parent.appendChild(newCard)
-    })
+// Import the filesystem module
+const fs = require('fs');
 
-}
+let data = "This is a file containing a collection"
+    + " of programming languages.\n"
+    + "1. C\n2. C++\n3. Python";
 
-//  add event listener to the button
-but.addEventListener('click', (e) => {
-    parent.innerHTML = ''
-    getData(getText.value)
-})
+fs.wite("./file/programming.txt", data, (a, b) => {
+    console.log(a, b)
+});
+console.log("File written successfully\n");
+console.log("The written has the following contents:");
+console.log(fs.readFileSync("./file/programming.txt", "utf8"));
