dfa3447ede44199f7af257ac96c2a4be927cd0b0
saikrishna <saikrishnacheruku4884@gmail.com>
2021-10-13 14:55:26 +0530

    recipe app

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+const button = document.querySelector('button')
+const input = document.querySelector('input')
 
-
-const callbackfunc = (event) => {
-    body.classList.toggle('dark')
+const getData = async (item) => {
+    console.log(item)
+    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log()
+    data.hits.forEach(recipe => {
+        console.log(recipe)
+    })
 }
 
-
-button.addEventListener('click', callbackfunc)
+//  add event listener to the button
+button.addEventListener('click', (e) => {
+    getData(input.value)
+})
