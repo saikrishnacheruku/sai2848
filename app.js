989b134e340073c480fdd82fe75c30ef1b4db8c5
saikrishna <saikrishnacheruku4884@gmail.com>
2021-10-13 15:00:59 +0530

    recipe app

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+const but = document.querySelector('button')
+const getText = document.querySelector('input')
+const parent = document.querySelector('div.grid')
 
 const getData = async (item) => {
     console.log(item)
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
     console.log(url)
     const res = await fetch(url)
     const data = await res.json()
-    console.log()
-    data.hits.forEach(recipe => {
-        console.log(recipe)
+    const recipies = data.hits
+    recipies.forEach(item => {
+        const { recipe } = item
+        const { label, image, source, calories } = recipe
+        const template = ` 
+                        <img class="card__image" src="${image}" />
+                        <div class="card__data">
+                            <div class="card__info">
+                                <h2>${label}</h2>
+                                <p>From ${source}</p>
+                            </div>
+                            <h3 class="card__price"></h3>
+                            <button class="card__add">+</button>
+                        </div>
+                    `
+        const newCard = document.createElement('article')
+        newCard.setAttribute('class', 'card')
+        newCard.setAttribute('style', 'margin-bottom:20px')
+        newCard.innerHTML = template
+        parent.appendChild(newCard)
     })
+
 }
 
 //  add event listener to the button
-button.addEventListener('click', (e) => {
-    getData(input.value)
+but.addEventListener('click', (e) => {
+    parent.innerHTML = ''
+    getData(getText.value)
 })
