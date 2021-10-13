57211393d851d73e860fcc87a34359392eb36dd5
saikrishna <saikrishnacheruku4884@gmail.com>
2021-10-13 14:41:41 +0530

    query selectors

--- app.js
+++ app.js
@@ -1,59 +1,14 @@
-// Operators
+var list = document.querySelector('ul')
 
 
-var a = 2
-var b = 10
+var arr = ['Go the GYM ', 'cook oatmeal', 'Eat']
 
-// Arithmetic operators
+arr.push('Complete the assignment')
 
-console.log('a + b =', a + b)
-console.log('b - a =', b - a)
-console.log('b / a =', b / a)
-console.log('b * a =', b * a)
-console.log('b ** a =', b ** a)
-console.log('b % a =', b % a)
+for (var i = 0; i < arr.length; i++) {
+    var element = document.createElement('li')
+    var textNode = document.createTextNode(arr[i])
+    element.appendChild(textNode)
+    list.appendChild(element)
 
-
-// Assignment Operators
-console.log('b ', b)
-b += 10
-console.log('b+=10 ', b)
-b++
-console.log('b++ ', b)
-b -= 10
-console.log('b-=10 ', b)
-b--
-console.log('b-- ', b)
-
-var c = 10
-
-console.warn(c++)
-console.log(c)
-// console.warn(++c)
-
-// Comparison operators
-
-var d = 10
-var e = 2
-var f = '10'
-
-console.log(d > e)
-console.log(d < e)
-console.log(d >= e)
-console.log(d <= e)
-console.log(d == e)
-
-console.warn(d == f)
-console.warn(d === f)
-
-// Logical operators
-
-console.log('true && true ', true && true)
-console.log('true && false', true && false)
-console.log('false && true', false && true)
-console.log('false && false', false && false)
-
-console.log('true || true ', true || true)
-console.log('true || false', true || false)
-console.log('false || true', false || true)
-console.log('false || false', false || false)
+}
