9ecc1e989136db1d9460fad76f93241098a04e6f
saikrishna <saikrishnacheruku4884@gmail.com>
2021-10-13 15:36:09 +0530

    packages

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-// Node.js program to demonstrate the
-// fs.writeFileSync() method
+const fs = require('fs')
 
-// Import the filesystem module
-const fs = require('fs');
 
-let data = "This is a file containing a collection"
-    + " of programming languages.\n"
-    + "1. C\n2. C++\n3. Python";
+// fs.readFile('./test.txt', (error, data) => {
+//     if (error) console.log(error)
+//     else console.log(data.toString())
+// })
 
-fs.wite("./file/programming.txt", data, (a, b) => {
-    console.log(a, b)
-});
-console.log("File written successfully\n");
-console.log("The written has the following contents:");
-console.log(fs.readFileSync("./file/programming.txt", "utf8"));
+// console.log(fs.readFileSync('./test.txt', 'utf8'))
+
+
+fs.writeFile('./test.txt', `Append me ${fs.readFileSync('./test.txt', 'utf8')}`, (error) => {
+    if (error) console.log(error)
+})
