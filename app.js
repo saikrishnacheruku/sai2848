9509c5862dda51bccd66a1bb308914a0fd1ef55c
sai krishna <saikrishnacheruku4884@gmail.com>
2021-10-24 10:54:01 +0530

    functions

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var list =document.querySelector('ul')
-var arr=['go to gym','cooking','eat']
-arr.push('submit assignment')
-for(var i=0;i<arr.length;i++){
-    var element =document.createElement('li')
-    var textnode=document.createTextNode(arr[i])
-    element.appendChild(textnode)
-    list.appendChild(element)
-    
+function greet( Name,age){
+    console.log(Name,age)
+    console.log("hello")
 }
+greet("sai",21)
+function add(a=0,b=0){
+    var sum=a+b
+    return sum
+}
+var add=function(a=0,b=0){
+    var sum=a+b
+    return sum
+}
+console.log(add(10,20))
